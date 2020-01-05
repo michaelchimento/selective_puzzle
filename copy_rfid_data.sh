c9db32b38a831c79538df96e38a8d188d784a74f
michael <mchimento@ab.mpg.de>
2020-01-05 11:49:09 +0100

    fixed directory structure

--- copy_rfid_data.sh
+++ copy_rfid_data.sh
@@ -2,5 +2,5 @@
 # observ_launcher.sh
 # launches correct python scripts with directory management
 
-cp -r /home/pi/Desktop/sonja_puzzle_rev2/data /media/pi/INTENSO/data &
+cp -r /home/pi/sonja_puzzlebox/data /media/pi/INTENSO/data &
 exit 0
--- puzzle_launcher.sh
+++ puzzle_launcher.sh
@@ -2,7 +2,7 @@
 # observ_launcher.sh
 # launches correct python scripts with directory management
 
-cd /home/pi/Desktop/sonja_puzzle_rev2/
+cd /home/pi/sonja_puzzlebox/
 sleep 10
 python3 sonja_puzzle_rev2.py&
 exit 0
