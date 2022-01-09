3b9b926621e10133eaaf1191f419d506fe5e37cb
Michael Chimento <mchimento@ab.mpg.de>
2022-01-09 11:22:01 +0100

    Update puzzle_launcher.sh

--- puzzle_launcher.sh
+++ puzzle_launcher.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
-# observ_launcher.sh
 # launches correct python scripts with directory management
 
 cd /home/pi/sonja_puzzlebox/
 sleep 15
-python3 sonja_puzzle_rev2.py&
+python3 main_puzzle.py&
 exit 0
