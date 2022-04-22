442ae826d9282f8dd9601b91221db1f7f911cc85
Michael Chimento <mchimento@ab.mpg.de>
2022-04-22 16:52:37 +0200

    Update copy_rfid_data.sh

--- copy_rfid_data.sh
+++ copy_rfid_data.sh
@@ -2,5 +2,5 @@
 # observ_launcher.sh
 # launches correct python scripts with directory management
 
-cp -r /home/pi/sonja_puzzlebox/data /media/pi/INTENSO &
+cp -r /home/pi/selective_puzzle/data /media/pi/INTENSO &
 exit 0
